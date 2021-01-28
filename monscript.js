8f28323841aad065dfaef9a332338c3b416c1fe1
Jeremy Lagier <jeremy.ljp97@gmail.com>
2021-01-28 01:47:48 +0100

    Modification affichage popup

--- monscript.js
+++ monscript.js
@@ -1,3 +1,25 @@
 function show() {
-  document.getElementById('popup').style.display = 'block';
+  const popup = document.getElementById('popup');
+  const p = document.querySelectorAll('p')[1];
+
+  if(popup.style.display === 'none'){
+    
+    popup.style.display='block';
+    p.style.color='red';
+  }
+    else{
+      popup.style.display='none';
+    }
+  
+
+  /*
+  if (popup.classList.contains('cache')){
+    popup.classList.remove('cache');
+    popup.classList.add('show');
+  }
+    else{
+      popup.classList.remove('show');
+      popup.classList.add('cache');
+    }
+  */
 }
